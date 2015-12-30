599a3a6e371e2221782d7848fbfb461f50780ca9
Eli Kohen Gomez <elikohen@gmail.com>
2015-12-30 22:42:53 +0100

    Updated FormatSpecifiers Matcher to order specifiers if there's one

--- helpers/FormatSpecifiers.rb
+++ helpers/FormatSpecifiers.rb
@@ -3,9 +3,11 @@ class FormatSpecifiers
     def initialize(string)
         @specifiers = []
         
-        matches = string.scan(/%(@|d|D|u|U|x|X|o|O|f|e|E|g|G|c|C|s|S|p|a|A|F|ld|lx|lu|zx)/)
+        matches = string.scan(/%(\d)?\$?(@|d|D|u|U|x|X|o|O|f|e|E|g|G|c|C|s|S|p|a|A|F|ld|lx|lu|zx)/)
+        matches.sort! { |a,b| a[0] <=> b[0] }
         matches.each { |match|
-            @specifiers.push(match[0])
+            puts "Found: #{match}"
+            @specifiers.push(match[1])
         }
     end
     
