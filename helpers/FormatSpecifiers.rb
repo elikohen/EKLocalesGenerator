031788a2f0bf2e220742d7b96b6374bd525ce968
Eli Kohen <eli@delitbee.com>
2021-08-28 17:05:30 +0200

    Added detection for {{element}} inside texts
    Mapped those elements as methods for swift functions

--- helpers/FormatSpecifiers.rb
+++ helpers/FormatSpecifiers.rb
@@ -2,20 +2,29 @@ class FormatSpecifiers
     attr_reader :specifiers
     def initialize(string)
         @specifiers = []
+        @tags = []
         
         matches = string.scan(/%(\d)?\$?(@|d|D|u|U|x|X|o|O|f|e|E|g|G|c|C|s|S|p|a|A|F|ld|lx|lu|zx)/)
         matches.sort! { |a,b| a[0] <=> b[0] }
         matches.each { |match|
             @specifiers.push(match[1])
         }
+        tagsmatch = string.scan(/\{\{(.*?)\}\}/)
+        tagsmatch.each { |match|
+            @tags.push(match[0])
+        }
     end
     
     def empty?
-        @specifiers.empty?
+        @specifiers.empty? && @tags.empty?
+    end
+
+    def has_tags?
+        !@tags.empty?
     end
     
     def ==(other)
-    self.class == other.class && self.specifiers == other.specifiers
+    self.class == other.class && self.specifiers == other.specifiers && self.tags == other.tags
     end
 
     def format_args
@@ -24,6 +33,9 @@ class FormatSpecifiers
             type = type_for_specifier(@specifiers[i])
             args.push("_ var#{i+1}: #{type}")
         end
+        @tags.each { |tag|
+            args.push("#{tag}: String")
+        }
         return args.join(", ")
     end
 
@@ -33,6 +45,14 @@ class FormatSpecifiers
             vars.push("var#{i+1}")
         end
         return vars.join(", ")
+    end
+
+    def replacement_tags
+        replacements = []
+        @tags.each { |tag|
+            replacements.push("replacingOccurrences(of: \"{{#{tag}}}\", with: #{tag})")
+        }
+        return replacements.join(".")
     end
 
     def type_for_specifier(specifier)
@@ -49,6 +69,7 @@ class FormatSpecifiers
     end
 
     def to_s
-        @specifiers.to_s
+        return @specifiers.to_s unless @specifiers.empty?
+        @tags.to_s
     end
 end
